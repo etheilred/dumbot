e529aef312e848e6cfbf185c4d8fce2ba90dcb69
roman <rdandrievskiy@edu.hse.ru>
2019-12-10 21:56:47 +0300

    fix

--- pkg/utils/uitls.go
+++ pkg/utils/uitls.go
@@ -4,14 +4,14 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 
 type UserPref struct {
 	ID int64
-	InlineKeyboard *tgbotapi.InlineKeyboardMarkup
-	Keyboard *tgbotapi.ReplyKeyboardMarkup
+	InlineKeyboard tgbotapi.InlineKeyboardMarkup
+	Keyboard tgbotapi.ReplyKeyboardMarkup
 }
 
 func (usr *UserPref) SetInlineKeyboard(buttons []tgbotapi.InlineKeyboardButton) {
-	*usr.InlineKeyboard = tgbotapi.NewInlineKeyboardMarkup(buttons)
+	usr.InlineKeyboard = tgbotapi.NewInlineKeyboardMarkup(buttons)
 }
 
 func (usr *UserPref) SetKeyboard(buttons []tgbotapi.KeyboardButton) {
-	*usr.Keyboard = tgbotapi.NewReplyKeyboard(buttons)
+	usr.Keyboard = tgbotapi.NewReplyKeyboard(buttons)
 }
